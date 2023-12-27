81c9765ccfa60b0b475a4aca7e4fe1de26cb61d7
Yuriy Yastrebov <netproblemmm@gmail.com>
2023-12-27 23:25:29 +0300

    add programm1

---
+++ 05/Seminar01/Programm1/Program.cs
@@ -0,0 +1,14 @@
+﻿Console.WriteLine("Введите 1 число: ");
+int firstNumber = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Введите 2 число: ");
+int secondNumber = Convert.ToInt32(Console.ReadLine());
+
+if (firstNumber == secondNumber * secondNumber)
+{
+    Console.WriteLine("Да, " + firstNumber + " является квадратом от " + secondNumber);
+}
+else
+{
+    Console.WriteLine("Нет, " + firstNumber + " не является квадратом от " + secondNumber);
+}
