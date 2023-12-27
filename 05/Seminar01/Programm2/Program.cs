a9c707ccc35f0260861eb4ca6ed4b76c918ce7ed
Yuriy Yastrebov <netproblemmm@gmail.com>
2023-12-28 01:48:09 +0300

    add programm2

---
+++ 05/Seminar01/Programm2/Program.cs
@@ -0,0 +1,9 @@
+﻿Console.WriteLine("Введите число: ");
+int N = Convert.ToInt32(Console.ReadLine());
+int negativeN = -N;
+
+while(negativeN <= N)
+{
+    Console.Write(negativeN + "\t");
+    negativeN++;
+}
