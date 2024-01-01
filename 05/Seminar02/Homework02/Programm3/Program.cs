f148b12721a956a7ab4d111de8bd2cb75c8ed8cd
Yuriy Yastrebov <netproblemmm@gmail.com>
2024-01-01 16:54:23 +0300

    add homework2:Programm3

---
+++ 05/Seminar02/Homework02/Programm3/Program.cs
@@ -0,0 +1,17 @@
+﻿int number = 0;
+while (number < 10 || number > 99)
+{
+    Console.WriteLine("Введите число от 10 до 99: ");
+    number = Convert.ToInt32(Console.ReadLine());
+}
+int firstNumber = number / 10;
+int secondNumber = number % 10;
+
+if (firstNumber > secondNumber)
+{
+    Console.WriteLine("Наибольшая цифра числа = " + firstNumber);
+}
+else
+{
+    Console.WriteLine("Наибольшая цифра числа = " + secondNumber);
+}
