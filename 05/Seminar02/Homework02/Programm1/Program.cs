b824324752c5e3bce6ccb5982c37527e5800e7f9
Yuriy Yastrebov <netproblemmm@gmail.com>
2024-01-01 16:02:10 +0300

    add homework1

---
+++ 05/Seminar02/Homework02/Programm1/Program.cs
@@ -0,0 +1,10 @@
+﻿Console.WriteLine("Введите число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+if (number % 7 == 0 && number % 23 == 0)
+{
+    Console.WriteLine("Число кратно 7 и 23");
+}
+else
+{
+    Console.WriteLine("Число НЕ кратно 7 и 23");
+}
