74e35877c1c3085b4c14e7504d96e89a09629e1c
Yuriy Yastrebov <netproblemmm@gmail.com>
2024-01-01 16:17:24 +0300

    add homework2

--- 05/Seminar02/Homework02/Programm2/Program.cs
+++ 05/Seminar02/Homework02/Programm2/Program.cs
@@ -1,2 +1,25 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.WriteLine("Введите число X: ");
+int x = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите число Y: ");
+int y = Convert.ToInt32(Console.ReadLine());
+int coordNumber = 0;
+
+if (x > 0 && y > 0)
+{
+    coordNumber = 1;
+}
+if (x < 0 && y > 0)
+{
+    coordNumber = 2;
+}
+if (x < 0 && y < 0)
+{
+    coordNumber = 3;
+}
+if (x > 0 && y < 0)
+{
+    coordNumber = 4;
+}
+
+Console.WriteLine("Номер координатной четверти = " + coordNumber);
+
