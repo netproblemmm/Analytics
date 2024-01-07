59e9af27d6b0cfa91aec1d6a837cf57a700db35e
Yuriy Yastrebov <netproblemmm@gmail.com>
2024-01-07 21:27:30 +0300

    add homework_2

---
+++ 05/Seminar02/Homework02/Programm4/Program.cs
@@ -0,0 +1,17 @@
+﻿Console.WriteLine("Введите число: ");
+string str = Convert.ToString(Console.ReadLine());
+
+int i = 0;
+while (i <= str.Length - 1)
+{
+    if (i < str.Length - 1)
+    {
+        Console.Write(str[i] + ", ");
+    }
+    else
+    {
+        Console.Write(str[i]);
+    }
+
+    i += 1;
+}
