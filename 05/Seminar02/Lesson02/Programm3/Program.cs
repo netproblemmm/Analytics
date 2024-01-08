fc3ca72666f0babe3632f4d39e9b1bd8a040f9a2
Yuriy Yastrebov <netproblemmm@gmail.com>
2024-01-08 16:09:56 +0300

    adding Lesson02: Programm3

---
+++ 05/Seminar02/Lesson02/Programm3/Program.cs
@@ -0,0 +1,25 @@
+﻿// Напишите программу, которая принимает на вход число и
+// возвращает 3 цифру с начала введенного числа
+// Примеры:
+// 12345678 => 3
+// 78671 => 6
+
+Console.Write("Введите число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+if(number > 99)
+{
+    int copyNumber = number;
+    
+    while(copyNumber > 999)
+    {
+        copyNumber /=10;
+    }
+    int result = (copyNumber % 10);
+
+    Console.WriteLine($"Результат: {number} => {result}");
+}
+else
+{
+    Console.WriteLine("Число не трехзначное или отрицательное");
+}
