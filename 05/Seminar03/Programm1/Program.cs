dde96da975e0c23a133f88de0b6173ce9ca20f40
Yuriy Yastrebov <netproblemmm@gmail.com>
2024-01-15 02:20:50 +0300

    add Seminar03: Programm1+2+3

---
+++ 05/Seminar03/Programm1/Program.cs
@@ -0,0 +1,29 @@
+﻿// Задайте массив. Напишите программу, которая определяет,
+// присутствует ли заданное число в массиве. Программа
+// должна выдать ответ: Да/Нет.
+// Примеры
+// [1 3 4 19 3], 8 => Нет
+// [-4 3 4 1], 3 => Да
+
+int[] array = {11, 22, 33, 44, 55};
+Console.Write("Введите число для поиска: ");
+int numberForSearch = Convert.ToInt32(Console.ReadLine());
+bool isFound = false;
+
+for(int i = 0; i < array.Length; i++)
+{
+    if(numberForSearch == array[i])
+    {
+        isFound = true;
+        break;
+    }
+}
+
+if(isFound)
+{
+    Console.Write("да");
+}
+else
+{
+    Console.Write("нет");
+}
---
+++ 05/Seminar03/Programm3/Program.cs
@@ -0,0 +1,24 @@
+﻿// Найдите произведения пар чисел в одномерном массиве. Парой
+// считаем первый и последний элемент, второй и предпоследний и
+// т.д. Результат запишите в новый массив.
+// Пример
+// [1 3 2 4 2 3] => [3 6 8]
+// [2 3 1 7 5 6 3] => [6 18 5] (элемент 7 не имеет пары)
+
+int[] array = {11, 22, 33, 44, 55, 66, 77};
+// Создаем массив на 3 элемента, причем [0, 0, 0]
+int[] result = new int[array.Length / 2];
+
+Console.WriteLine($"Массив: [ {string.Join(" | ", array)} ]");
+
+for(int i = 0, j = array.Length - 1; i < result.Length; i++)
+{
+    result[i] = array[i] * array[j];
+}
+
+Console.WriteLine($"Результат: [ {string.Join(" | ", result)} ]");
+
+if (array.Length % 2 == 1)
+{
+Console.Write($" (Число {array[array.Length / 2]} НЕ имеет пары)");
+}
