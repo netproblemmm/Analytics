f8cae1a6e6ea4666073c524c29c09d09e91843ba
Yuriy Yastrebov <netproblemmm@gmail.com>
2024-01-14 23:37:50 +0300

    add Lesson3: Programm2

---
+++ 05/Lesson03/Programm2/Program.cs
@@ -0,0 +1,14 @@
+﻿void PrintSquares(int limit)
+{
+    int i = 1;
+    while(i <= limit)
+    {
+        Console.Write($"{i * i} ");
+        i++;
+    }
+    Console.WriteLine("");
+}
+
+PrintSquares(5);
+PrintSquares(10);
+PrintSquares(15);
