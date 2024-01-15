e8ab0f9b55e7fd7360ae04587a6f87dfc1af56bd
Yuriy Yastrebov <netproblemmm@gmail.com>
2024-01-15 22:12:22 +0300

    add Homework3: Task 1,2,3

---
+++ 05/Seminar03/Homework3/Task1/Program.cs
@@ -0,0 +1,61 @@
+﻿// Задайте одномерный массив из 10 целых чисел от 1 до 100.
+// Найдите количество элементов массива, значения которых лежат в отрезке [10,90].
+
+using System;
+using System.Linq;
+
+//Тело класса будет написано студентом. Класс обязан иметь статический метод PrintResult()
+class UserInputToCompileForTest
+{
+    // Подсчет количества элементов массива, попадающих в заданный диапазон
+    // numbers - массив, в котором ведется подсчет
+    // minRange - минимальная граница диапазона
+    // maxRange - максимальная граница диапазона
+    public static int CountItemsRange(int[] numbers, int minRange, int maxRange)
+    {
+        int countNumbers = 0;
+        foreach (int e in numbers)
+        {
+            if (e >= minRange && e <= maxRange)
+                countNumbers++;
+        }
+        return countNumbers;
+    }
+
+    public static void PrintResult(int[] array)
+    {
+        Console.WriteLine(CountItemsRange(array, 10, 90));
+    }
+
+}
+
+
+//Не удаляйте и не меняйте класс Answer!
+class Answer
+{
+    public static void Main(string[] args)
+    {
+        int[] array;
+
+
+        if (args.Length >= 1)
+        {
+            // Объединяем все аргументы командной строки в одну строку
+            string joinedArgs = string.Join(" ", args);
+
+            // Разделяем строку по запятой с пробелом и преобразуем в массив целых чисел
+            array = joinedArgs.Split(", ")
+                                  .Select(int.Parse)
+                                  .ToArray();
+
+            // Теперь arr содержит преобразованные в целые числа из командной строки
+
+        }
+        else
+        {
+            // Если аргументов на входе нет
+            array = new int[] { 1, 5, 10, 20, 30, 40, 99, 4, 90, 3 }; // Создание массива
+        }
+        UserInputToCompileForTest.PrintResult(array);
+    }
+}
---
+++ 05/Seminar03/Homework3/Task3/Program.cs
@@ -0,0 +1,69 @@
+﻿// Задайте массив из вещественных чисел с ненулевой дробной частью.
+// Найдите разницу между максимальным и минимальным элементов массива.
+
+using System;
+using System.Linq;
+
+//Тело класса будет написано студентом. Класс обязан иметь статический метод PrintResult()
+class UserInputToCompileForTest
+{ 
+    // Разница между максимальным и минимальным элементами массива
+    
+    // Нахождение минимума массива
+    public static double FindMin(double[] numbers)
+    {
+        double minNumber = numbers[0];
+        foreach (double e in numbers)
+        {
+            if (e < minNumber)
+                minNumber = e;
+        }
+        return minNumber;
+    }
+    
+    // Нахождение максимума массива
+    public static double FindMax(double[] numbers)
+    {
+        double maxNumber = numbers[0];
+        foreach (double e in numbers)
+        {
+            if (e > maxNumber)
+                maxNumber = e;
+        }
+        return maxNumber;
+    }
+    
+    
+    
+    public static void PrintResult(double[] array)
+    {
+        Console.WriteLine(FindMax(array) - FindMin(array));
+    }
+}
+
+//Не удаляйте и не меняйте класс Answer!
+class Answer
+{
+    public static void Main(string[] args)
+    {
+        double[] array;
+        
+
+        if (args.Length >= 1) {
+            // Объединяем все аргументы командной строки в одну строку
+            string joinedArgs = string.Join(" ", args);
+
+            // Разделяем строку по запятой с пробелом и преобразуем в массив целых чисел
+            array = joinedArgs.Split(", ")
+                                  .Select(double.Parse)
+                                  .ToArray();
+            
+            // Теперь arr содержит преобразованные в целые числа из командной строки
+        
+        } else {
+           // Если аргументов на входе нет
+            array = new double[] {0.25, 5.4, 1.3, 2.1, 3.8, 5.2, 3.01}; // Создание массива
+        }
+        UserInputToCompileForTest.PrintResult(array);
+    }
+}
