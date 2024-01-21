622562aa59e30df0faae6cb33b05d715fee119c3
Yuriy Yastrebov <netproblemmm@gmail.com>
2024-01-22 00:18:32 +0300

    add Homework04

---
+++ 05/Seminar04/Homework04/Task2/Program.cs
@@ -0,0 +1,20 @@
+﻿// Задайте массив заполненный случайными трёхзначными числами.
+// Напишите программу, которая покажет количество чётных чисел в массиве.
+
+int size = 10;
+int[] array = new int[size]; // Массив array на size элементов
+for (int i = 0; i < array.Length; i++)
+{
+    array[i] = new Random().Next(100, 1000);
+    // 100,1000: 100-999
+}
+
+int evenNumberQuantity = 0;
+foreach (int e in array)
+{
+    if (e % 2 == 0)
+    evenNumberQuantity += 1;
+}
+
+Console.WriteLine($"Массив: [ {string.Join("; ", array)} ]");
+Console.WriteLine($"Количество четных номеров: {evenNumberQuantity}");
---
+++ 05/Seminar04/Homework04/Task3/Program.cs
@@ -0,0 +1,19 @@
+﻿// Напишите программу, которая перевернёт одномерный массив (первый элемент станет последним, второй – предпоследним и т.д.)
+
+int size = 10;
+int[] array1 = new int[size]; // Массив array на size элементов
+for (int i = 0; i < size; i++)
+{
+    array1[i] = new Random().Next(1, 10);
+    // 100,1000: 100-999
+}
+
+int[] array2 = new int[size]; // Массив array на size элементов
+
+for (int i = 0; i < size; i++)
+{
+    array2[size - 1 - i] = array1[i];
+}
+
+Console.WriteLine($"Массив: [ {string.Join("; ", array1)} ]");
+Console.WriteLine($"Массив: [ {string.Join("; ", array2)} ]");
