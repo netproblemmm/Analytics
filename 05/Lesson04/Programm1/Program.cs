6c5e61306762a17eec2fa66efa8f34d082b86376
Yuriy Yastrebov <netproblemmm@gmail.com>
2024-01-28 21:28:06 +0300

    add Lesson04: Programm1

---
+++ 05/Lesson04/Programm1/Program.cs
@@ -0,0 +1,29 @@
+﻿int [,] matrix = CreateMatrix(4, 5);
+ShowMatrix(matrix);
+
+int[,] CreateMatrix(int rowCount, int columnCount)
+{
+    int [,] matrix = new int [rowCount, columnCount];
+    Random rnd = new Random();
+
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            matrix [i, j] = rnd.Next(1, 11);
+        }
+    }
+    return matrix;
+}
+
+void ShowMatrix(int[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            Console.Write($"{matrix[i, j]} ");
+        }
+        Console.WriteLine();
+    }
+}
