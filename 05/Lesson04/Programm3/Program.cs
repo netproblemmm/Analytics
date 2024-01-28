e836cc61332ebb1ee4d42110324f20a24eb68c16
Yuriy Yastrebov <netproblemmm@gmail.com>
2024-01-28 23:24:52 +0300

    add Lesson04: Programm3

---
+++ 05/Lesson04/Programm3/Program.cs
@@ -0,0 +1,16 @@
+﻿string GetLettersFromString(string s)
+{
+    string letters = "";
+    foreach(char e in s)
+    {
+        if (char.IsAsciiLetter(e))
+        {
+            letters += e;
+        }
+    }
+    return letters;
+}
+
+string str = Console.ReadLine();
+string result = GetLettersFromString(str);
+Console.WriteLine(result);
