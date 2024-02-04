a09c5b04bec1e6648158d2a710b5c268171ceba8
Yuriy Yastrebov <netproblemmm@gmail.com>
2024-02-05 00:30:33 +0300

    add Seminar06: Homework

---
+++ 05/Seminar06/Homework2/Program.cs
@@ -0,0 +1,13 @@
+﻿// Задайте строку, содержащую латинские буквы в обоих регистрах.
+// Сформируйте строку, в которой все заглавные буквы заменены на строчные. 
+// "aBcDefG" -> "abcdefg"
+using System;
+
+string str = "aBcDefG";
+Console.WriteLine(GetStringToLower(str));
+
+string GetStringToLower(string str)
+{
+    str = str.ToLower();
+    return str;
+}
---
+++ 05/Seminar06/Homework3/Program.cs
@@ -0,0 +1,27 @@
+﻿// Задайте произвольную строку. Выясните, является ли она палиндромом.
+using System;
+
+string str = string.Empty;
+str = Console.ReadLine();
+
+if (IsPalindrome(str))
+{
+    Console.WriteLine("Yes");
+}
+else
+{
+    Console.WriteLine("No");
+}
+
+bool IsPalindrome (string str)
+{
+    for (int i = 0; i < str.Length; i++)
+    {
+        Console.WriteLine(str[i]);
+        if (str[i] != str[str.Length - 1 - i])
+        {
+            return false;
+        }
+    }
+    return true;
+}
