0350f6b8abda238417077819961631451b227438
Yuriy Yastrebov <netproblemmm@gmail.com>
2025-02-24 23:07:39 +0300

    Quarter_1_lesson_1

---
+++ 01-01/main.py
@@ -0,0 +1,35 @@
+n = 9
+print(n, 8, 6)
+n = 'dsfdf'
+#print(type(n))
+n = 'ds\'fdf'
+print(n)
+
+a = 5
+b = 5.89
+c = 'hello'
+print(f"{a} - {b} - {c}")
+print("{} - {} - {}".format(a,b,c))
+
+# print("Введите первое число:")
+# a = input()
+# b = input("Введите первое число: ")
+# print(a, ' + ', b, a+b)
+
+c = 5.893
+print(c)
+n = int(c)
+print(n)
+
+c = 3
+c = bool(c)
+print(c)
+
+# print("Введите первое число:")
+# a = int(input())
+# b = int(input("Введите первое число: "))
+# print(a, ' + ', b, ' = ', a+b)
+
+a = 5.8888
+b = 6.55545
+print(round(a*b, 3))
