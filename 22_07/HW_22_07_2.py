fee5f5738483de6d41735e8abb188635f2dfe5d9
Yuriy Yastrebov <netproblemmm@gmail.com>
2025-07-21 01:51:24 +0300

    22-07_HW2

--- 22_07/HW_22_07_2.py
+++ 22_07/HW_22_07_2.py
@@ -8,3 +8,27 @@ e. принимать диапазон сохраняемого оригинал
  Например для диапазона [3, 6] берутся буквы с 3 по 6 из исходного имени файла.
  К ним прибавляется желаемое конечное имя, если оно передано. Далее счётчик файлов и расширение.
 '''
+# import os
+from pathlib import Path
+
+def rename_group(new_name="New", num=2, extension="txt", extension_new="doc", old_name=[1, 5]):
+    """Функция переименовывает группу файлов.
+        new_name - желаемое новое имя
+        num - колличество знаков в числе
+    """
+    no_rename_extension = ["md", "py"]  # расширения которые не переименовываем, чтоб не подломать пока тестирую
+    p = Path(Path().cwd())
+    i = 0
+    for obj in p.iterdir():
+        if obj.is_file():
+            if obj.name.split('.')[-1] == extension:
+                if extension not in no_rename_extension:
+                    Path(obj).rename(f'{obj.name.split('.')[0][old_name[0]:old_name[1]]}_{new_name}_{str(i).
+                                            zfill(num)}.{extension_new}')
+                    i += 1
+                else:
+                    print(f'{extension} - запрещено к переименованию')
+
+
+if __name__ == '__main__':
+    rename_group("itog", 5, "doc", "txt", [0, 3])
